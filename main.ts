ee3a1d5463414c301502a7c0132f8a96ae560bd1
TheM1rk <74301709+TheM1rk@users.noreply.github.com>
2022-05-27 20:45:46 +0200

    modifying the pinch/kick/thing mechanism's code

--- main.ts
+++ main.ts
@@ -1,79 +1,75 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == 1) {
-        maqueen.servoRun(maqueen.Servos.S2, 47)
-        basic.pause(215)
-        maqueen.servoRun(maqueen.Servos.S2, 75)
-    } else if (receivedNumber == 2) {
-        maqueen.servoRun(maqueen.Servos.S1, 75)
-        basic.pause(215)
-        maqueen.servoRun(maqueen.Servos.S1, 47)
-    } else if (receivedNumber == 3) {
-        maqueen.servoRun(maqueen.Servos.S2, 47)
-        maqueen.servoRun(maqueen.Servos.S1, 75)
-        basic.pause(215)
-        maqueen.servoRun(maqueen.Servos.S2, 75)
-        maqueen.servoRun(maqueen.Servos.S1, 47)
+    while (receivedNumber != 0) {
+        if (receivedNumber == 1) {
+            maqueen.servoRun(maqueen.Servos.S2, 90)
+            basic.pause(150)
+        } else if (receivedNumber == 2) {
+            maqueen.servoRun(maqueen.Servos.S1, 40)
+            basic.pause(150)
+        } else if (receivedNumber == 3) {
+            maqueen.servoRun(maqueen.Servos.S2, 90)
+            maqueen.servoRun(maqueen.Servos.S1, 40)
+            basic.pause(200)
+        }
+        maqueen.servoRun(maqueen.Servos.S1, 80)
+        maqueen.servoRun(maqueen.Servos.S2, 5)
     }
 })
 radio.onReceivedString(function (receivedString) {
     XandY = receivedString.split(" ", 2);
+joystickY = parseInt(XandY[1].trim(), 10);
+joystickX = parseInt(XandY[0], 10);
+if (joystickX != 500) {
+        velX = 510 / 1023 * joystickX - 255
+    } else {
+        velX = 0
+    }
+    if (joystickY != 500) {
+        velY = 510 / 1023 * joystickY - 255
+    } else {
+        velY = 0
+    }
 })
-let velX = 0
 let velY = 0
-radio.setGroup(111)
+let velX = 0
+radio.setGroup(40)
 radio.setFrequencyBand(83)
 let XandY = ["500", "500"]
 let joystickY: number, joystickX: number
 basic.forever(function () {
-    if (velY > 0) {
+    serial.writeLine("X =" + velX)
+    serial.writeLine("Y =" + velY)
+    if (velY >= 20) {
         if (velX > 0) {
-            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, velY - velX)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, Math.max(velY, velX) - Math.min(velY, velX))
             maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, velY)
         } else if (velX < 0) {
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, velY + velX)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, Math.max(velY, velX * -1) - Math.min(velY, velX * -1))
             maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, velY)
-        } else if (velX == 0) {
+        } else {
             maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, velY)
-        } else {
-            maqueen.motorStop(maqueen.Motors.All)
         }
-    } else if (velY == 0) {
+    } else if (velY <= -20) {
         if (velX > 0) {
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, velX)
-        } else if (velX < 0) {
-            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, velX * -1)
-        } else {
-            maqueen.motorStop(maqueen.Motors.All)
-        }
-    } else if (velY < 0) {
-        if (velX > 0) {
-            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, velY * -1 - velX)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, Math.max(velY * -1, velX) - Math.min(velY * -1, velX))
             maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, velY * -1)
         } else if (velX < 0) {
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, velY * -1 + velX)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, Math.max(velY * -1, velX * -1) - Math.min(velY * -1, velX * -1))
             maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, velY * -1)
-        } else if (velX == 0) {
+        } else {
             maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, velY * -1)
+        }
+    } else if (velX != 0 && velY > -20 && velY < 20) {
+        if (velX > 0) {
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, velX / 2)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, velX / 2)
+        } else if (velX < 0) {
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, velX * -1 / 2)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, velX * -1 / 2)
         } else {
             maqueen.motorStop(maqueen.Motors.All)
         }
     } else {
         maqueen.motorStop(maqueen.Motors.All)
-    }
-})
-control.inBackground(function () {
-    while (true) {
-        joystickY = parseInt(XandY[1].trim(), 10);
-joystickX = parseInt(XandY[0], 10);
-if (joystickX != 500) {
-            velX = 510 / 1023 * joystickX - 255
-        } else {
-            velX = 0
-        }
-        if (joystickY != 500) {
-            velY = 510 / 1023 * joystickY - 255
-        } else {
-            velY = 0
-        }
     }
 })
